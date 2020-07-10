4b74a5c69d71c8a60931ebdeea6ba7add9b3d042
Sadanand Singh <sadanand4singh@gmail.com>
2020-07-10 11:10:34 -0700

    update fish configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,13 +6,15 @@ set -g theme_display_git_untracked yes
 set -g theme_display_git_dirty yes
 set -g theme_display_nvm yes
 set -g theme_display_virtualenv yes
-set -g theme_color_scheme zenburn
+set -g theme_color_scheme dark
 
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
-eval /Users/sadanand/opt/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+eval /Users/sadanand/anaconda3/bin/conda "shell.fish" "hook" $argv | source
 # <<< conda initialize <<<
 
 # kill the right prompt __conda_add_prompt 😠
 function __conda_add_prompt
 end
+
+starship init fish | source
--- .config/fish/functions/py36.fish
+++
@@ -1,4 +0,0 @@
-function py36 -d 'activate py36 conda env'
-    conda activate py36-dev
-end
-
